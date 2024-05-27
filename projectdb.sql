c8f34869bebb5f4a2af33f4beaf7944dc47a9ade
MNG3664 <garbanazeer@gmail.com>
2024-05-27 13:21:43 +0100

    -

--- projectdb.sql
+++ projectdb.sql
@@ -1,135 +1,234 @@
-create database projectdb ;
-use projectdb;
-show tables;
+CREATE DATABASE  IF NOT EXISTS `projectdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `projectdb`;
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: projectdb
+-- ------------------------------------------------------
+-- Server version	8.0.36
 
-CREATE TABLE Admin (
-    admin_ID INT AUTO_INCREMENT PRIMARY KEY,
-    user_name VARCHAR(255),
-    password VARCHAR(255),
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    email VARCHAR(255)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE Student (
-    student_ID INT AUTO_INCREMENT PRIMARY KEY,
-    user_name VARCHAR(255),
-    password VARCHAR(255),
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    supervisor_ID INT,
-    FOREIGN KEY (supervisor_ID) REFERENCES Supervisor(supervisor_ID)
-);
+--
+-- Table structure for table `admin`
+--
 
-CREATE TABLE Student_Details (
-    student_ID INT PRIMARY KEY,
-    email VARCHAR(255),
-    FOREIGN KEY (student_ID) REFERENCES Student(student_ID)
-);
+DROP TABLE IF EXISTS `admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admin` (
+  `admin_ID` int NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`admin_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE Supervisor (
-    supervisor_ID INT AUTO_INCREMENT PRIMARY KEY,
-    user_name VARCHAR(255),
-    password VARCHAR(255),
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    email VARCHAR(255)
-);
+--
+-- Dumping data for table `admin`
+--
 
-CREATE TABLE Project (
-    project_ID INT AUTO_INCREMENT PRIMARY KEY,
-    project_name VARCHAR(255),
-    project_description TEXT,
-    admin_ID INT,
-    supervisor_ID INT,
-    student_ID INT,
-    FOREIGN KEY (admin_ID) REFERENCES Admin(admin_ID),
-    FOREIGN KEY (supervisor_ID) REFERENCES Supervisor(supervisor_ID),
-    FOREIGN KEY (student_ID) REFERENCES Student(student_ID)
-);
+LOCK TABLES `admin` WRITE;
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+INSERT INTO `admin` VALUES (1,'admin1','adminpassword1','Admin','One','admin1@example.com'),(3,'admin2','123','Admin','Two','admin2@example.com');
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE Comment (
-    comment_ID INT AUTO_INCREMENT PRIMARY KEY,
-    comment_text TEXT,
-    date DATE,
-    supervisor_ID INT,
-    student_ID INT,
-    project_ID INT,
-    FOREIGN KEY (supervisor_ID) REFERENCES Supervisor(supervisor_ID),
-    FOREIGN KEY (student_ID) REFERENCES Student(student_ID),
-    FOREIGN KEY (project_ID) REFERENCES Project(project_ID)
-);
+--
+-- Table structure for table `comment`
+--
 
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comment` (
+  `comment_ID` int NOT NULL AUTO_INCREMENT,
+  `comment_text` text,
+  `date` date DEFAULT NULL,
+  `supervisor_ID` int DEFAULT NULL,
+  `student_ID` int DEFAULT NULL,
+  `project_ID` int DEFAULT NULL,
+  PRIMARY KEY (`comment_ID`),
+  KEY `supervisor_ID` (`supervisor_ID`),
+  KEY `student_ID` (`student_ID`),
+  KEY `project_ID` (`project_ID`),
+  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`supervisor_ID`) REFERENCES `supervisor` (`supervisor_ID`),
+  CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`student_ID`) REFERENCES `student` (`student_ID`),
+  CONSTRAINT `comment_ibfk_3` FOREIGN KEY (`project_ID`) REFERENCES `project` (`project_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `comment`
+--
 
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+INSERT INTO `comment` VALUES (1,'Great job on the project!','2024-04-15',1,1,1),(2,'Great job on the project!','2024-04-15',1,1,1);
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
 
-INSERT INTO Admin (user_name, password, first_name, last_name, email) 
-VALUES ('admin1', 'adminpassword1', 'Admin', 'One', 'admin1@example.com');
+--
+-- Table structure for table `project`
+--
 
+DROP TABLE IF EXISTS `project`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `project` (
+  `project_ID` int NOT NULL AUTO_INCREMENT,
+  `project_name` varchar(255) DEFAULT NULL,
+  `project_description` text,
+  `admin_ID` int DEFAULT NULL,
+  `supervisor_ID` int DEFAULT NULL,
+  `student_ID` int DEFAULT NULL,
+  `filepath` varchar(2000) DEFAULT NULL,
+  PRIMARY KEY (`project_ID`),
+  KEY `admin_ID` (`admin_ID`),
+  KEY `supervisor_ID` (`supervisor_ID`),
+  KEY `student_ID` (`student_ID`),
+  CONSTRAINT `project_ibfk_1` FOREIGN KEY (`admin_ID`) REFERENCES `admin` (`admin_ID`),
+  CONSTRAINT `project_ibfk_2` FOREIGN KEY (`supervisor_ID`) REFERENCES `supervisor` (`supervisor_ID`),
+  CONSTRAINT `project_ibfk_3` FOREIGN KEY (`student_ID`) REFERENCES `student` (`student_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `project`
+--
 
-INSERT INTO Supervisor (user_name, password, first_name, last_name, email) 
-VALUES ('supervisor1', 'supervisorpassword1', 'Supervisor', 'One', 'supervisor1@example.com');
+LOCK TABLES `project` WRITE;
+/*!40000 ALTER TABLE `project` DISABLE KEYS */;
+INSERT INTO `project` VALUES (1,'Project One','Description for Project One',1,1,1,'C:\\Users\\USER\\Downloads\\Documents\\Research of final year project\\Implementation\\uploads'),(2,'Project management system',NULL,1,1,1,'C:\\Users\\USER\\Downloads\\Documents\\Research of final year project\\Implementation\\uploads');
+/*!40000 ALTER TABLE `project` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `sessions`
+--
 
-INSERT INTO Student (user_name, password, first_name, last_name, supervisor_ID) 
-VALUES ('student1', 'studentpassword1', 'Student', 'One', 1);
+DROP TABLE IF EXISTS `sessions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sessions` (
+  `session_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `expires` int unsigned NOT NULL,
+  `data` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
+  PRIMARY KEY (`session_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-INSERT INTO Student (user_name, password, first_name, last_name, supervisor_ID) 
-VALUES ('abc', '123', 'John', 'Doe', 1);
+--
+-- Dumping data for table `sessions`
+--
 
-ALTER TABLE student
-ADD COLUMN email VARCHAR(255);
+LOCK TABLES `sessions` WRITE;
+/*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `student`
+--
 
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student` (
+  `student_ID` int NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `supervisor_ID` int DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`student_ID`),
+  KEY `supervisor_ID` (`supervisor_ID`),
+  CONSTRAINT `student_ibfk_1` FOREIGN KEY (`supervisor_ID`) REFERENCES `supervisor` (`supervisor_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `student`
+--
 
-DELETE FROM Student
-WHERE student_ID = 7;
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+INSERT INTO `student` VALUES (1,'student1','studentpassword1','Student','One',1,'student1@email.com'),(6,'abc','123','John','Doe',1,'123@email.com'),(9,'mng2810','nazeer2810','Muhammad-Nazeer','Garba',2,'garbanazeer@gmail.com'),(10,'john','123','John','Doe',1,'doe@gnail.com');
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
 
-DELETE FROM Admin
-WHERE password = '123';
+--
+-- Table structure for table `student_details`
+--
 
+DROP TABLE IF EXISTS `student_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student_details` (
+  `student_ID` int NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`student_ID`),
+  CONSTRAINT `student_details_ibfk_1` FOREIGN KEY (`student_ID`) REFERENCES `student` (`student_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `student_details`
+--
 
+LOCK TABLES `student_details` WRITE;
+/*!40000 ALTER TABLE `student_details` DISABLE KEYS */;
+INSERT INTO `student_details` VALUES (1,'student1@example.com');
+/*!40000 ALTER TABLE `student_details` ENABLE KEYS */;
+UNLOCK TABLES;
 
-INSERT INTO Student_Details (student_ID, email) 
-VALUES (1, 'student1@example.com');
+--
+-- Table structure for table `supervisor`
+--
 
+DROP TABLE IF EXISTS `supervisor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `supervisor` (
+  `supervisor_ID` int NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`supervisor_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-INSERT INTO Project (project_name, project_description, admin_ID, supervisor_ID, student_ID) 
-VALUES ('Project One', 'Description for Project One', 1, 1, 1);
+--
+-- Dumping data for table `supervisor`
+--
 
+LOCK TABLES `supervisor` WRITE;
+/*!40000 ALTER TABLE `supervisor` DISABLE KEYS */;
+INSERT INTO `supervisor` VALUES (1,'supervisor1','supervisorpassword1','Supervisor','One','supervisor1@example.com'),(2,'hauwa','123','Huawa','Ibrahim-Aminu','hauwa@example.com');
+/*!40000 ALTER TABLE `supervisor` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-INSERT INTO Comment (comment_text, date, supervisor_ID, student_ID, project_ID) 
-VALUES ('Great job on the project!', '2024-04-15', 1, 1, 1);
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-
-SELECT p.project_name, COUNT(c.comment_ID) AS num_comments
-FROM Project p
-LEFT JOIN Comment c ON p.project_ID = c.project_ID
-GROUP BY p.project_name;
-
-
-select* from student;
-select* from admin;
-select* from supervisor;
-select* from project;
-select* from comment;
-
-UPDATE student
-SET email = '123@email.com'
-WHERE student_id = 6; 
-
-
-
-
-SELECT plugin FROM mysql.user WHERE user='root';
-
-
-
-ALTER USER 'root'@'127.0.0.1' IDENTIFIED WITH mysql_native_password BY 'nazeer3664';
-
-SELECT host, user, plugin FROM mysql.user;
-
-
+-- Dump completed on 2024-05-27 13:20:35
